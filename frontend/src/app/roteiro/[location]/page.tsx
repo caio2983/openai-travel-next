17ad05ae3b1222bb68e55eb2652bedf5d5314a99
caio2983 <caioluna2002@gmail.com>
2025-08-08 13:57:21 -0300

    feat(frontend): update location page

--- frontend/src/app/roteiro/[location]/page.tsx
+++ frontend/src/app/roteiro/[location]/page.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function LocationPage() {
-  return <div>Location</div>;
+  const tasks = [
+    {
+      title: "Visitar a Torre Eiffel",
+      description:
+        "Não deixe de conhecer a famosa Torre Eiffel e apreciar a vista panorâmica da cidade.",
+      image_query: "Torre Eiffel Paris",
+    },
+    {
+      title: "Passear pelo Louvre",
+      description:
+        "Explore o Museu do Louvre e admire obras de arte renomadas, como a Mona Lisa.",
+      image_query: "Louvre Paris",
+    },
+    {
+      title: "Caminhar às margens do Rio Sena",
+      description:
+        "Desfrute de um agradável passeio às margens do Rio Sena e aprecie a paisagem parisiense.",
+      image_query: "Rio Sena Paris",
+    },
+  ];
+
+  return <div className="container">Location</div>;
 }
