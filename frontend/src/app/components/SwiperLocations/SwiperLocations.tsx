f1ab8ad676a4878f50ebeddb4b77ea16bf3cb67c
caio2983 <caioluna2002@gmail.com>
2025-08-08 15:39:14 -0300

    feat(frontend): update swiper

--- frontend/src/app/components/SwiperLocations/SwiperLocations.tsx
+++ frontend/src/app/components/SwiperLocations/SwiperLocations.tsx
@@ -2,13 +2,13 @@
 
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import {
   A11y,
   Navigation,
   Pagination,
   Parallax,
   Scrollbar,
+  Autoplay,
 } from "swiper/modules";
 
 import "swiper/css";
@@ -20,70 +20,61 @@ export default function SwiperLocations() {
   return (
     <div className="swiper-container">
       <Swiper
-        modules={[Navigation, A11y, Parallax]}
-        spaceBetween={50}
+        modules={[Navigation, A11y, Parallax, Autoplay]}
+        spaceBetween={30}
         slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+        loop={true}
+        speed={7000}
+        freeMode={true}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        allowTouchMove={false}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
       >
         <SwiperSlide>
           <div className="slide-content">
             <h3>Paris</h3>
-            <div
-              className="image-teste"
-              style={{
-                width: "300px",
-                height: "200px",
-                backgroundColor: "green",
-              }}
-            ></div>
+            <div className="image-teste"></div>
             <p>Visite a Torre Eiffel e aproveite a gastronomia local.</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slide-content">
             <h3>Roma</h3>
-            <div
-              className="image-teste"
-              style={{
-                width: "300px",
-                height: "200px",
-                backgroundColor: "green",
-              }}
-            ></div>
+            <div className="image-teste"></div>
             <p>Explore o Coliseu e a rica história romana.</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slide-content">
             <h3>Nova York</h3>
-            <div
-              className="image-teste"
-              style={{
-                width: "300px",
-                height: "200px",
-                backgroundColor: "green",
-              }}
-            ></div>
+            <div className="image-teste"></div>
             <p>Conheça a Times Square e o Central Park.</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slide-content">
             <h3>Tóquio</h3>
-            <div
-              className="image-teste"
-              style={{
-                width: "300px",
-                height: "200px",
-                backgroundColor: "green",
-              }}
-            ></div>
-
+            <div className="image-teste"></div>
             <p>Descubra a cultura japonesa e a modernidade da cidade.</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <h3>Rio de Janeiro</h3>
+            <div className="image-teste"></div>
+            <p>Visite o Cristo Redentor e as belas praias.</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <h3>Dubai</h3>
+            <div className="image-teste"></div>
+            <p>Conheça o Burj Khalifa e o luxo da cidade.</p>
           </div>
         </SwiperSlide>
       </Swiper>
