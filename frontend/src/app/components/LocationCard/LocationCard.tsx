baab397af804310201a0ada8a9ba08dba2b807bf
caio2983 <caioluna2002@gmail.com>
2025-08-08 20:40:44 -0300

    feat(frontend): update location card

--- frontend/src/app/components/LocationCard/LocationCard.tsx
+++ frontend/src/app/components/LocationCard/LocationCard.tsx
@@ -7,10 +7,18 @@ export default function LocationCard({
   image_query,
 }: LocationCardProps) {
   return (
-    <div className="location-card">
-      <h3 className="location-title">{title}</h3>
-      <p className="location-description">{description}</p>
-      <p className="location-image-query">Imagem relacionada: {image_query}</p>
+    <div className="location-result">
+      <div className="location-card">
+        <h3 className="location-title">{title}</h3>
+        <p className="location-description">{description}</p>
+        <p className="location-image-query">
+          Imagem relacionada: {image_query}
+        </p>
+      </div>
+
+      <div className="location-image">
+        <div className="location-image-wrapper"></div>
+      </div>
     </div>
   );
 }
