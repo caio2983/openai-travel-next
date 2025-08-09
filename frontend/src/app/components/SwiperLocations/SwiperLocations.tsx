71e778ac896577f83d388fe04b1fe5a8e6872ad0
caio2983 <caioluna2002@gmail.com>
2025-08-08 21:09:52 -0300

    feat(frontend): update home page and swiper

--- frontend/src/app/components/SwiperLocations/SwiperLocations.tsx
+++ frontend/src/app/components/SwiperLocations/SwiperLocations.tsx
@@ -2,21 +2,51 @@
 
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  A11y,
-  Navigation,
-  Pagination,
-  Parallax,
-  Scrollbar,
-  Autoplay,
-} from "swiper/modules";
+import { A11y, Navigation, Parallax, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-export default function SwiperLocations() {
+export default function SwiperLocations({
+  setBackground,
+}: {
+  setBackground: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const locations = [
+    {
+      title: "Paris",
+      img: "/paris.jpg",
+      desc: "Visite a Torre Eiffel e aproveite a gastronomia local.",
+    },
+    {
+      title: "Roma",
+      img: "/rome.jpg",
+      desc: "Explore o Coliseu e a rica história romana.",
+    },
+    {
+      title: "Nova York",
+      img: "/newyork.jpg",
+      desc: "Conheça a Times Square e o Central Park.",
+    },
+    {
+      title: "Tóquio",
+      img: "/tokyo.jpg",
+      desc: "Descubra a cultura japonesa e a modernidade da cidade.",
+    },
+    {
+      title: "Rio de Janeiro",
+      img: "/rio.jpg",
+      desc: "Visite o Cristo Redentor e as belas praias.",
+    },
+    {
+      title: "Dubai",
+      img: "/dubai.jpg",
+      desc: "Conheça o Burj Khalifa e o luxo da cidade.",
+    },
+  ];
+
   return (
     <div className="swiper-container">
       <Swiper
@@ -34,49 +64,23 @@ export default function SwiperLocations() {
         allowTouchMove={false}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
+        draggable={true}
       >
-        <SwiperSlide>
-          <div className="slide-content">
-            <h3>Paris</h3>
-            <div className="image-teste"></div>
-            <p>Visite a Torre Eiffel e aproveite a gastronomia local.</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <h3>Roma</h3>
-            <div className="image-teste"></div>
-            <p>Explore o Coliseu e a rica história romana.</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <h3>Nova York</h3>
-            <div className="image-teste"></div>
-            <p>Conheça a Times Square e o Central Park.</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <h3>Tóquio</h3>
-            <div className="image-teste"></div>
-            <p>Descubra a cultura japonesa e a modernidade da cidade.</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <h3>Rio de Janeiro</h3>
-            <div className="image-teste"></div>
-            <p>Visite o Cristo Redentor e as belas praias.</p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <h3>Dubai</h3>
-            <div className="image-teste"></div>
-            <p>Conheça o Burj Khalifa e o luxo da cidade.</p>
-          </div>
-        </SwiperSlide>
+        {locations.map((loc, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="slide-content"
+              onMouseEnter={() => setBackground(`${loc.img}`)}
+              onMouseLeave={() => setBackground("")}
+            >
+              <h3>{loc.title}</h3>
+              <div className="slide-image-wrapper">
+                <img src={loc.img} alt={loc.title} />
+              </div>
+              <p>{loc.desc}</p>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import SwiperLocations from "./components/SwiperLocations/SwiperLocations";
 import Input from "./components/Input/Input";
+import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
+  const [background, setBackground] = useState<string>("");
+
   return (
-    <div className="container">
+    <div className="container home-container">
+      <div className="image-background">
+        {background !== "" && (
+          <Image
+            src={background}
+            alt="Imagem de fundo home page"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        )}
+      </div>
       <Input></Input>
-      <SwiperLocations></SwiperLocations>
+      <SwiperLocations setBackground={setBackground}></SwiperLocations>
     </div>
   );
 }
